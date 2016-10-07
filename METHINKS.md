5647442439ad98ec139936e78f7a9fcd9fd265df
Mike Evans <mike.evans@numical.com>
2016-10-07 20:33:49 +0100

    added basket creation facade

--- METHINKS.md
+++ METHINKS.md
@@ -19,6 +19,23 @@
 * Off to discovery testing...
 * [Set it up](https://github.com/numical/shoppingbasket/tree/start-of-discovery-testing)
 * So what dos the client (aka test, GUI etc) require?
- * an item catalogue
- * a shopping basket
-* So mock these ...
+ * an item catalogue (global)
+ * a shopping basket (instance)
+* [So mock
+these](https://github.com/numical/shoppingbasket/tree/discovery-testing-01-core-responsibilities)
+
+## What type of Object Orientation?
+* This is JavaScript so have options here - functional, OLOO, OO..?
+* Using a bit of epxperience here, let's use factories to hide 'new' and 'this' ugliness but use
+classes within these factories for conceptual clarity...
+
+## Discount Logic
+* When considering factories, a tricky issue arose:
+* Who is responsible for discount logic?  How is this introduced?
+ * a discount engine that injects a discount function on basket creation
+* This led to a function chain for basket creation - so wrap in a Facade...
+
+## Discover Relationships
+* So now time to explore the iteraction between these core concerns
+* Time to switch discovery tests to more like user-story-driven acceptance tests
+
--- spec/discoveryTests.js
+++ spec/discoveryTests.js
@@ -7,19 +7,41 @@ require('jasmine2-custom-message');
 
 const td = require('testdouble');
 
-const itemCatalogue = td.replace('../lib/itemCatalogue.js');
-const shoppingBasket = td.replace('../lib/shoppingBasket.js');
+let itemCatalogue;
+let createShoppingBasket;
+let generateDiscountFunction;
+let ShoppingBasket;
 
-describe('Discovery Tests ', () => {
-  it('client requires item catalogue', () => {
-    expect(itemCatalogue).toBeDefined();
+describe('Discovery Testing', () => {
+  describe('Core Concerns ', () => {
+    beforeEach(() => {
+      itemCatalogue = require('../lib/itemCatalogue.js');
+      createShoppingBasket = require('../lib/basketFactory.js');
+      generateDiscountFunction = require('../lib/discountEngine.js');
+    });
+    it('client requires item catalogue', () => {
+      expect(itemCatalogue).toBeDefined();
+    });
+    it('client requires factory function for shopping baskets', () => {
+      expect(createShoppingBasket).toBeDefined();
+    });
+    it('shopping basket creation requires discount engine', () => {
+      expect(generateDiscountFunction).toBeDefined();
+    });
   });
+  describe('Basket Facade', () => {
+    beforeEach(() => {
+      createShoppingBasket = td.replace('../lib/basketFactory.js');
+      generateDiscountFunction = td.replace('../lib/discountEngine.js');
+      ShoppingBasket = require('../lib/ShoppingBasket.js');
+    });
+    it('Facade wraps basket creation', () => {
+      let discountFn = () => 42;
+      td.when(generateDiscountFunction(false)).thenReturn(discountFn);
 
-  it('client requires shopping basket', () => {
-    expect(shoppingBasket).toBeDefined();
-  });
+      ShoppingBasket.create(false);
 
-  afterEach(() => {
-    td.reset();
+      td.verify(createShoppingBasket(discountFn));
+    });
   });
 });
